fbb12cac16389d549383c40a678891134cf323c1
JiYeoungIll <jeff01279@gmail.com>
2022-01-21 17:28:58 +0900

    2cam

--- 2cam.py
+++ 2cam.py
@@ -2,7 +2,7 @@ import cv2
 import threading
 
 capture = cv2.VideoCapture(0)
-cap = cv2.VideoCapture(cv2.CAP_DSHOW+1)
+cap = cv2.VideoCapture(1)  #cv2.CAP_DSHOW+1혹은 1로 지정
 running = True
 
 def run1():
@@ -12,26 +12,16 @@ def run1():
         capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         cv2.imshow("original1", frame1)
-        if cv2.waitKey(1) == ord('q'):
-            return -1
-    capture.release()
-    cv2.destroyAllWindows()
-def run2():
-
-    while running:
         ret2, frame2 = cap.read()
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         cv2.imshow("original2", frame2)
         if cv2.waitKey(1) == ord('q'):
-                return -1
+            return -1
     cap.release()
+    capture.release()
     cv2.destroyAllWindows()
 
 th1 = threading.Thread(target=run1)
-th2 = threading.Thread(target=run2)
 th1.start()
-th2.start()
 print("started..")
-
-
