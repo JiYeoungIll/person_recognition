6928e52b77d130bbb2bace37229094c6c389b615
JiYeoungIll <jeff01279@gmail.com>
2022-02-14 13:46:06 +0900

    1.2

--- final.py
+++ final.py
@@ -1,3 +1,4 @@
+
 # 졸업작품
 # 지능형 CCTV
 import cv2
@@ -10,11 +11,15 @@ from PyQt5 import QtGui
 
 ###
 from imutils.video import FPS
+import argparse # 명령행 파싱(인자를 입력 받고 파싱, 예외처리 등) 모듈
+import imutils # 파이썬 OpenCV가 제공하는 기능 중 복잡하고 사용성이 떨어지는 부분을 보완(이미지 또는 비디오 스트림 파일 처리 등)
+import os # 운영체제 기능 모듈
 import argparse  # 명령행 파싱(인자를 입력 받고 파싱, 예외처리 등) 모듈
 import imutils  # 파이썬 OpenCV가 제공하는 기능 중 복잡하고 사용성이 떨어지는 부분을 보완(이미지 또는 비디오 스트림 파일 처리 등)
 import os  # 운영체제 기능 모듈
 
 fps = FPS().start()
+
 
 # 네트워크 불러오기 - cv2.dnn.readNet
 # OpenCv로 딥러닝을 실행하기 위해서는 일단 cv2.dnn.readNet 클래스 객체 생성
@@ -23,6 +28,7 @@ fps = FPS().start()
 # model : 훈련된 가중치를 저장하고 있는 파일
 # confing : 구성파일. 알고리즘에 관한 모든 설정
 net = cv2.dnn.readNet("yolo/yolov4.weights", "yolo/yolov4.cfg")
+
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 # 객체 이름 가져오는 부분
@@ -30,39 +36,36 @@ classes = []
 with open("yolo/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
-output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 # 색상(굳이 필요 없어보임) - 사람 인식할때 그려지는 박스 색상 -- 색상 아닌듯 함
 # np.random.unifrom은 NumPy에서 제공하는 균등분포 함수이다.
 # 최소값, 최대값, 데이터 개수 순서로 Parameter를 입력해준다.
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
 # 내장웹캠 연결
 cap = cv2.VideoCapture(0)
-
 # 외부웹캠 연결
-# cap = cv2.VideoCapture(0)
-# instantiate a variable 'p' to keep count of persons
-# 숫자를 세기위해 p변수 사용 -- 사용 안해도 될듯
-# p = 0
+#cap = cv2.VideoCapture(0)
+
 # initialize the writer
 # 초기화
 writer = None
 starting_time = time.time()
 frame_id = 0
 running = False
+
 # ROI 설정을 위한 마우스 상태, 좌표 초기화
 mouse_is_pressing = False
 start_x, end_x, start_y, end_y = 0, 0, 0, 0
 step = 0
-temp = 0
 
+#Color 윈도우창 생성 초기화
+roi_on = False
+redrectangle = False
 
-# ROI 설정을 위해 두개의 변수 값을 바꿔주는 함수
-def swap(v1, v2):
-    global temp
-    v1, v2 = v2, v1
-    return (v1, v2)
-
+# counting 수 초기
+count = 0
 
 # Press The Left Button Of Mouse == Start Position Of ROI
 # Release The Left Button Of Mouse == End Position Of ROI
@@ -89,29 +92,22 @@ def Mouse_Callback(event, x, y, flags, param):
         end_x = x
         end_y = y
 
-
-aaaa = False
-
-# counting 수 초기
-count = 0
-
-
 def run():
+    global redrectangle
     global count
-    global aaaa
+    global roi_on
     global running
-    (W, H) = (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     global label2
-    global start_x, start_y, end_x, end_y, step
+    global start_x , start_y, end_x, end_y, step
+    (W, H) = (cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
     while running:
-        ret, img = cap.read()
-        # img = imutils.resize(img, width=1500)
-        count = 0
         global frame_id
+        ret, img = cap.read()
+        #img = imutils.resize(img, width=1500)
+        count = 0
         frame_id += 1
 
-        # if W is None or H is None:
-        # (H, W) = img.shape[:2]
         # 네트워크 입력 블롭 만들기 - cv2.dnn.blob.FromImage
         # 객체 탐지 부분
         # 입력 영상을 블롭객체로 만들어 추론을 진행 ( 블롭이란? 이진 스케일로 연결된 픽셀 그룹 )
@@ -119,27 +115,26 @@ def run():
         # scalefactor = 딥러닝 학습 진행할 때, 입력 영상을 픽셀값으로 했는지 정규화 이용했는지 맞게 지정
         # size : 학습할 때, 사용한 영상의 크기
         if step == 3:
-
-            # if aaaa==True:
             frm = img.copy()
             frm = frm[start_y: end_y, start_x: end_x]
             print(frm.shape)
-
             if frame_id % 5 == 1:
-                blob = cv2.dnn.blobFromImage(frm, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-                (H, W) = frm.shape[:2]
-
-        else:
+             blob = cv2.dnn.blobFromImage(frm , 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+             (H, W) = frm.shape[:2]
+        else :
             (H, W) = img.shape[:2]
             blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
         # 네트워크 입력 설정하기
         net.setInput(blob)
         # 네트워크 순방향 실행(추론)
         outs = net.forward(output_layers)
+
         # initialize our lists of detected bounding boxes, confidences, and class IDs, respectively
         boxes = []
         confidences = []
         class_ids = []
+
         # loop over each of the layer outputs
         for out in outs:
             # loop over each of the detections
@@ -183,82 +178,100 @@ def run():
                 (w, h) = (boxes[i][2], boxes[i][3])
                 label = str(classes[class_ids[i]])
                 if label == 'person':
-
                     # counting 수 증가
                     count += 1
-                    # print("person{}".format(frame_id))
+                    #print("person{}".format(frame_id))
                     # 클래스 ID 및 확률
                     text = "{} : {:.2f}%".format(classes[class_ids[i]], confidences[i])
                     # label text 잘림 방지
                     y = y - 15 if y - 15 > 15 else y + 15
                     # text 출력
                     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
                 else:
                     continue
+
                 # draw a bounding box rectangle and label on the frame
                 # color : 배열 나옴 [B,G,R]
                 color = [int(c) for c in colors[class_ids[i]]]
                 # rectangle(검출영역, 시작점, 종료점, 색상,선굵기 : -1일경우 내부선그리기)
-                if aaaa == False:
+                if roi_on == False:
                     cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
                     text = label
                     # putText(프레임,텍스트,문자열 위치, 폰트,폰트 크기, 색상,굵기)
                     cv2.putText(img, text, (x, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-                else:
-                    # img = img[start_y: end_y, start_x: end_x]
-                    cv2.rectangle(img, (start_x + x, start_y + y), (x + start_x + w, y + start_y + h), color, 2)
+                else :
+                    #img = img[start_y: end_y, start_x: end_x]
+                    cv2.rectangle(img, (start_x+x, start_y+y), (x+start_x + w, y+start_y + h), color, 2)
                     text = label
                     # putText(프레임,텍스트,문자열 위치, 폰트,폰트 크기, 색상,굵기)
-                    cv2.putText(img, text, (start_x + x, start_y + y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0),
-                                2)
+                    cv2.putText(img, text, (start_x+x, start_y+y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
         # counting 결과 출력
         counting_text = "People Counting : {}".format(count)
         cv2.putText(img, counting_text, (10, img.shape[0] - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+        #elapsed_time = time.time() - starting_time
+        #fps = frame_id / elapsed_time
+        #print(str(round(fps, 2)))
 
-        # elapsed_time = time.time() - starting_time
-        # fps = frame_id / elapsed_time
-        # print(str(round(fps, 2)))
-
+        if redrectangle:
+            cv2.namedWindow("Color")
+            cv2.setMouseCallback("Color", Mouse_Callback)
         # 파이큐티
-        if ret:
-            # Press The Left Button
-            if step == 1:
-                cv2.circle(img, (start_x, start_y), 10, (0, 255, 0), -1)
-            # Moving The Mouse
-            elif step == 2:
-                cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 3)
-            # Release Of The Mouse
-            elif step == 3:
-                # If Start X Position Is Bigger Than End X
+            if ret:
+                # Press The Left Button
+                if step == 1:
+                    cv2.circle(img, (start_x, start_y), 10, (0, 255, 0), -1)
+                # Moving The Mouse
+                elif step == 2:
+                    cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 3)
+                # Release Of The Mouse
+                elif step == 3:
+                    # If Start X Position Is Bigger Than End X
+                    if start_x > end_x and start_y < end_y:  # 오른쪽 위에서 왼쪽 아래로 드래그 할 시\
+                        if end_x < 0:
+                            end_x = 0
+                        start_x, end_x = end_x, start_x
+                    elif start_x > end_x and start_y > end_y:  # 오른쪽 아래서 왼쪽 위로 드래그 할 시
+                        if end_y < 0:
+                            end_y = 0
+                        start_y, end_y = end_y, start_y
+                        print(start_y)
+                        start_x, end_x = end_x, start_x
+                    elif start_x < end_x and start_y > end_y:  # 왼쪽 아래서 오른쪽 위로 드래그시
+                        if end_y < 0:
+                            end_y = 0
+                        start_y, end_y = end_y, start_y
 
-                if start_x > end_x and start_y < end_y:      #오른쪽 위에서 왼쪽 아래로 드래그 할 시\
-                    if end_x < 0:
-                        end_x = 0
-                    start_x, end_x = end_x, start_x
-                elif start_x > end_x and start_y > end_y:        #오른쪽 아래서 왼쪽 위로 드래그 할 시
-                    if end_y < 0:
-                        end_y = 0
-                    start_y, end_y = end_y, start_y
-                    print(start_y)
-                    start_x, end_x = end_x, start_x
-                elif start_x < end_x and start_y > end_y:       #왼쪽 아래서 오른쪽 위로 드래그시
-                    if end_y < 0:
-                        end_y = 0
-                    start_y, end_y = end_y, start_y
+                    ROI = img[start_y: end_y, start_x: end_x]
+                    if count == 0 :
+                        ROI = cv2.add(ROI, (200, 50, 50, 0))
+                        #ROI = cv2.add(ROI, (200, 200, 50, 0))
+                    elif count == 1 :
+                        ROI = cv2.add(ROI, (0, 65, 200, 0))
+                    else:
+                        ROI  = cv2.add(ROI, (50, 50, 200, 0))
+                    img[start_y: end_y, start_x: end_x] = ROI
+                    roi_on = True
 
+                cv2.imshow("Color", img)
+                key = cv2.waitKey(1)
+                #esc 누를경우
+                if key == 27:
+                    #break
+                    cv2.destroyWindow("Color")
+                    redrectangle =False
+
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                h, w, c = img.shape
+                qImg = QtGui.QImage(img.data, w, h, w * c, QtGui.QImage.Format_RGB888)
+                pixmap = QtGui.QPixmap.fromImage(qImg)
+                label2.setPixmap(pixmap)
+
+        elif redrectangle == False:
+            if not (start_x == 0 and end_x == 0 and start_y == 0 and end_y == 0) :
                 ROI = img[start_y: end_y, start_x: end_x]
-                ROI = cv2.add(ROI, (50, 50, 100, 50))
+                ROI = cv2.add(ROI, (50, 50, 200, 0))
                 img[start_y: end_y, start_x: end_x] = ROI
-                aaaa = True
-            cv2.namedWindow("ROI")
-            cv2.setMouseCallback("ROI", Mouse_Callback)
-            cv2.imshow("ROI", img)
-            key = cv2.waitKey(1)
-            # esc 누를경우
-            if key == 27:
-                break
 
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             h, w, c = img.shape
@@ -270,43 +283,50 @@ def run():
             QtWidgets.QMessageBox.about(win, "Error", "Cannot read frame.")
             print("cannot read frame.")
             break
+
     cv2.destroyWindow()
     writer.release()
     cap.release()
     print("Thread end.")
 
-
 def stop():
     global running
     running = False
     print("stoped..")
-
-
 def start():
     global running
     running = True
     th = threading.Thread(target=run)
     th.start()
     print("started..")
-
-
+def red():
+    global redrectangle
+    redrectangle = True
+    print("REDREDREDRED..")
 def onExit():
     print("exit")
     stop()
 
-
 app = QtWidgets.QApplication([])
 win = QtWidgets.QWidget()
 vbox = QtWidgets.QVBoxLayout()
+
 label2 = QtWidgets.QLabel()
 btn_start = QtWidgets.QPushButton("카메라 켜기")
 btn_stop = QtWidgets.QPushButton("카메라 끄기")
+red_roi = QtWidgets.QPushButton("배회영역 설정")
+
 vbox.addWidget(label2)
 vbox.addWidget(btn_start)
 vbox.addWidget(btn_stop)
+vbox.addWidget(red_roi)
+
 win.setLayout(vbox)
 win.show()
+
 btn_start.clicked.connect(start)
 btn_stop.clicked.connect(stop)
+red_roi.clicked.connect(red)
+
 app.aboutToQuit.connect(onExit)
 sys.exit(app.exec_())
